eedcb9a7fcd195504f40d44e4ff0f2ae3ba850dd
lixiaolong <lxl@qq.com>
2017-01-25 05:08:40 +0800

    补充提交

---
+++ SvduPro/SVListView/SVDefineVarConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using SVCore;
+
+namespace SVControl
+{
+    public class SVDefineVarConverter : TypeConverter
+    {
+        /// <summary>
+        /// 重写父类，这里不需要关注
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="sourceType"></param>
+        /// <returns></returns>
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            SVVarDefine varDefine = value as SVVarDefine;
+            if (varDefine == null)
+                return base.ConvertTo(context, culture, value, destinationType);
+
+            if (String.IsNullOrWhiteSpace(varDefine.VarName))
+                return "None";
+
+            return varDefine.VarName;
+        }
+    }
+}
