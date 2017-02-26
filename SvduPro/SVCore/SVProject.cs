1407af6fb96eec5328e1ca767523985b5c34b638
lixiaolong <lxl@qq.com>
2017-02-27 07:54:12 +0800

    提交 数据库

--- SvduPro/SVCore/SVProject.cs
+++ SvduPro/SVCore/SVProject.cs
@@ -169,8 +169,8 @@ namespace SVCore
             SVLog.TextLog.Listeners().Add(textListener);
             SVLog.WinLog.Listeners().Add(textListener);
 
-            //_sqlDataBase.connect(SVProData.dbIp, SVProData.ProName, SVProData.user, SVProData.passwd);
-            _sqlDataBase.connect(SVProData.dbIp, "sv", SVProData.user, SVProData.passwd);
+            _sqlDataBase.connect(SVProData.dbIp, SVProData.ProName, SVProData.user, SVProData.passwd);
+            //_sqlDataBase.connect(SVProData.dbIp, "sv", SVProData.user, SVProData.passwd);
             //SVProData.DownLoadFile = _sqlDataBase.getProPath();
             ///先不从数据库中获取下装文件生成的路径
             SVProData.DownLoadFile = Path.Combine(SVProData.ProPath, "Download");
